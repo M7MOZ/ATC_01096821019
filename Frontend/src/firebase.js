9dae520f891383e9728c476f218327b26d2e7f04
Mahmoud Ahmed <M7MOZ>
2025-05-14 03:02:03 +0300

    added api keys to .env file

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDf1JDbTZvhXLhMM1hV9FJJbfeJKjiucc",
-  authDomain: "areeb-fa792.firebaseapp.com",
-  projectId: "areeb-fa792",
-  storageBucket: "areeb-fa792.firebasestorage.app",
-  messagingSenderId: "107661581147",
-  appId: "1:107661581147:web:b33f9d8e7c42451a74587e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
